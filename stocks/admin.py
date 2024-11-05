813f14dbd944004593941dca74a7a573c381ecc0
ninocsta <nino-csta@hotmail.com>
2024-11-05 08:42:34 -0300

    adicao do stocks

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from stocks.models import Stock
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'moment',)
+    search_fields = ('name',)
+    list_filter = ('name',)
