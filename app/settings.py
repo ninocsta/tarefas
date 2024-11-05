38d0058ede27f5a573836acc8657fc85976ea949
ninocsta <nino-csta@hotmail.com>
2024-11-05 08:29:36 -0300

    static

--- app/settings.py
+++ app/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/python/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/tarefas/public/static'
 
 # Default primary key field type
